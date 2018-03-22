d4b20dd2f6b79aca089e82b71d6e5cc06ba01320
David <dshvimer@protonmail.com>
2018-03-22 12:54:06 -0400

    init repo

---
+++ s1c1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string in = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    cout << "hello" << endl;
+    return 0;
+}
