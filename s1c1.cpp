3cc34d1f13683b0447d1b795cb54fa8a74049a58
David <dshvimer@protonmail.com>
2018-03-22 14:45:22 -0400

    use sstream to parse hex

--- s1c1.cpp
+++ s1c1.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
-using namespace std;
+#include <boost/archive/iterators/base64_from_binary.hpp>
 
 int main() {
-    string in = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    cout << "hello" << endl;
+    std::string in = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    unsigned int raw;   
+    std::stringstream input;
+    input << std::hex << in;
+    input >> raw;
     return 0;
 }
