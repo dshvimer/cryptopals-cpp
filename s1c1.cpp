7e1ad648ccc708dcfdba8c83b961a73c3bd15956
David <dshvimer@protonmail.com>
2018-03-23 21:27:31 -0400

    use vector<bool?

--- s1c1.cpp
+++ s1c1.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <bitset>
+#include <vector>
 
 using namespace std;
 
 const int NIBBLE_SIZE = 4;
 const int BASE64_SIZE = 6;
 
-void hex_to_base64(const string hex_input)
+typedef vector<unsigned char> bytes;
+typedef vector<bool> raw_data;
+
+bytes parse_hex(const string hex_input)
 {
     string padded_input;
-    stringstream data, output;
-    int iterations;
-    int bit_count = hex_input.length() * NIBBLE_SIZE;
-    int base64_len = bit_count / BASE64_SIZE;
-    int rmdr = bit_count % BASE64_SIZE;
+    stringstream hex_reader;
+    bytes data;
+    unsigned char value;
 
+    //Padded to be easily
+    int bit_count = hex_input.length() * NIBBLE_SIZE;
+    int rmdr = bit_count % BASE64_SIZE;
     switch(rmdr)
     {
         case 2:
@@ -29,22 +33,28 @@ void hex_to_base64(const string hex_input)
             padded_input = hex_input;
     }
     
-    data << hex << padded_input;
-
-    iterations = padded_input.length() / 3;
-    for (int i = 0; i < iterations; i++) {
-        unsigned char buf[3];
-        unsigned char digit;
-        
-        data.read((char*)buf, 3);
-        digit = buf[0];
-        cout << bitset<8>(digit) << endl;
-        digit <<= 2;
-        cout << bitset<8>(digit) << endl;
+    for (char& c : padded_input) {
+        cout << c << endl;
+        unsigned char byte;
+        hex_reader << hex << c;
+        hex_reader >> byte;
+        data.push_back(byte);
     }
+
+    for (int i : data) {
+        cout << i << endl;
+    }
+
+    return data;
+}
+
+string bytes_to_base64(bytes data)
+{
+    string out;
+    
 }
 
 int main() {
-    hex_to_base64("FFF");
+    bytes data = parse_hex("03F");
     return 0;
 }
