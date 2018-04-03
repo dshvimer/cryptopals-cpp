ad43a3e1535b08e0314a07f7399161fbd78b2d82
David <dshvimer@protonmail.com>
2018-04-02 22:48:47 -0400

    write binary to base64

--- s1c1.cpp
+++ s1c1.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ const int NIBBLE_SIZE = 4;
 const int BASE64_SIZE = 6;
 
 typedef vector<unsigned char> bytes;
-typedef vector<bool> raw_data;
+typedef vector<bool> binary_data;
 
 bytes parse_hex(const string hex_input)
 {
@@ -45,32 +46,59 @@ bytes parse_hex(const string hex_input)
     return data;
 }
 
-raw_data bytes_to_raw(bytes data)
+binary_data bytes_to_binary(bytes data)
 {
-    raw_data raw;
+    binary_data raw;
     int j;
     for (int c : data) { //cast to int from unsigned char
-        cout << c << endl;
+        //cout << c << endl;
         for (int i = 0; i < 4; i++) {
             j = c & 0x8;
             j >>= 3;
             c <<= 1;
-            cout << " " << j;
-
+            //cout << " " << j;
             raw.push_back(j);
         }
-        cout << endl;
+        //cout << endl;
     }
 
     for (bool i : raw) {
-        cout << i << endl;
+        cout << i;
     }
+    cout << endl;
 
     return raw;
 }
 
+string binary_to_base64(binary_data raw)
+{
+    stringstream out;
+    queue<bool> buf;
+    int j, k, ch;
+    int limit = raw.size() / 6;
+
+    for (bool b : raw) {
+        buf.push(b);
+    }
+
+    for (int i = 0; i < limit; i++) {
+        ch = 0;
+        for (j = 5; j >= 0; j--) {
+            k = buf.front();
+            ch |= k;
+            cout << k << " | 0 = " << ch << endl;
+            ch <<= 1;
+            buf.pop();
+        }
+        cout << ch << endl;
+    }
+
+    return "";
+}
+
 int main() {
-    bytes data = parse_hex("3F");
-    raw_data raw = bytes_to_raw(data);
+    bytes data = parse_hex("AF0");
+    binary_data raw = bytes_to_binary(data);
+    // binary_to_base64(raw);
     return 0;
 }
